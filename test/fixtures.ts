5f5f784492781e72395ed0158ee9b05de14f087e
Sergey Peshkov <gpuzzletime@icloud.com>
2021-06-29 23:54:42 +0300

    master: add tests for callbackData event

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -40,3 +40,33 @@ export function getEditedMessageEvent(text: string = '') {
 		},
 	} as const;
 }
+
+export function getCallbackQueryEvent(queryId: string) {
+	return {
+		type: 'callbackQuery',
+		payload: {
+			queryId,
+			from: {
+				userId: '1@user',
+				firstName: 'User',
+				lastName: 'Name',
+			},
+			message: {
+				from: {
+					userId: '1@user',
+					firstName: 'User',
+					lastName: 'Name',
+				},
+				chat: {
+					chatId: '1@chat',
+					type: 'group',
+					title: 'Admin Chat',
+				},
+				msgId: '1',
+				text: 'some message',
+				timestamp: Math.floor(Date.now() / 1000),
+			},
+			callbackData: 'some data',
+		}
+	} as const;
+}
