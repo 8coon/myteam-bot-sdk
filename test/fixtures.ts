a56e29ea3bf822d11db96896fc0d14fa7c4edde7
Sergey Peshkov <gpuzzletime@icloud.com>
2021-06-29 22:44:42 +0300

    master: add more tests

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -18,3 +18,25 @@ export function getNewMessageEvent(text: string = '') {
 		},
 	} as const;
 }
+
+export function getEditedMessageEvent(text: string = '') {
+	return {
+		type: 'editedMessage',
+		payload: {
+			from: {
+				userId: '1@user',
+				firstName: 'User',
+				lastName: 'Name',
+			},
+			chat: {
+				chatId: '1@chat',
+				type: 'group',
+				title: 'Admin Chat',
+			},
+			msgId: '1',
+			text,
+			timestamp: Math.floor(Date.now() / 1000),
+			editedTimestamp: Math.floor(Date.now() / 1000),
+		},
+	} as const;
+}
